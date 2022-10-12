e7392a2ebeb1992b5d687562b35be0000cc6bf80
yuicchicode <Yuicchisama@gmail.com>
2022-10-11 23:03:17 -0300

    Functionalitys update

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVANDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone With Camera',
+        productNumber: '32157',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '11855',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'HighTech Master 4k Drobe',
+        productNumber: '92245',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Heli 8k Camera',
+        productNumber: '66987',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
